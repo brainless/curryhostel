c9e0a6e7d805a723714f46351dcfc5c01c1ed4e5
Sumit Datta <sumitdatta@gmail.com>
2024-04-10 13:48:03 +0530

    Added Summer Retreat 2024, about, costs, costs (retreat) pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,31 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Curry Hostel",
+      social: {
+        instagram: "https://instagram.com/curryhostel",
+      },
+      sidebar: [
+        {
+          label: "Basics",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Idea and space", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
+    }),
+  ],
 });
